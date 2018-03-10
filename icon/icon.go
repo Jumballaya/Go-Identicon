10980a0b91d216170ea3604caaa52ba6b0c45703
Patrick Burris <patrick@briefmedia.com>
2018-03-09 21:41:46 -0600

    start to refactor

--- icon/icon.go
+++ icon/icon.go
@@ -1,135 +1,156 @@
 package icon
 
+// @TODO: I need to refactor buildHalfMap it still feels clumsy and I
+//				am not so sure that It needs to be a method anyway. It
+//        should be refactored into smaller functions.
+//        This also needs to be tighter when it comes to typing
+
+// @TODO: I should mirror the half_map and set PixelMap to 6x5
+//        before getting the the Render method.
+//
+//        Simplify the Render method by moving most of the meat to
+//        functions and better utilize SquareSize, Width and Height.
+//        Set the color to the color type. Change the Hex property or
+//        get rid of it, I am not sure I really need it.
+//
+//        Allow the CLI tool to override the color, square size, height,
+//        width or padding.
+
+// @TODO: Really need to refactor the creation of the img.Pix array. I need to really
+//        learn the way it is built and how to actually form the slice (array?)
+//
 
 import (
-  "encoding/hex"
-  "crypto/md5"
-  "strings"
-  "strconv"
-  "image"
-  "image/color"
-  "image/png"
-  "bytes"
+	"bytes"
+	"crypto/md5"
+	"encoding/hex"
+	"image"
+	"image/color"
+	"image/png"
+	"strconv"
+	"strings"
 )
-
 
 // Identicon Struct
 type Identicon struct {
-  Input string
-  Hex string
-  Color []string
-  Pixelmap [3][5]bool
-  Padding int
-  SquareSize int
-  Width int
-  Height int
+	Input      string
+	Hex        string
+	Color      []string
+	Pixelmap   [3][5]bool
+	Padding    int
+	SquareSize int
+	Width      int
+	Height     int
 }
 
-
-// Identicon.CreateHex()
+// makeHex()
 // Creates 32 character hex string from the Identicon's input
-func (icon *Identicon) CreateHex() {
-  data := []byte(icon.Input)
-  sum := md5.Sum(data)
-  hex := hex.EncodeToString(sum[:])
-  icon.Hex = hex
-}
-
-
-// Identicon.CreateMap()
-// Creates a nested boolean array
-func (icon *Identicon) CreateMap() {
-  letters := strings.Split(icon.Hex, "")
-  hex_array := []bool{}
-  // Pair the letters by two's and convert the hex (e.g. f8) to integer
-  for i := 0; i < len(letters); i = i + 2 {
-    digit := letters[i: i + 2]
-    num, _ := strconv.ParseInt(digit[0], 16, 32)
-
-    // This is the criteria for which squares get color
-    // Could be anything else e.g.
-    // (num < 4 || num > 14) || (num % 3 == 0)
-    hex_array = append(hex_array, num % 2 == 0)
-  }
-  hex_array = hex_array[:15]
-
-  // Build the grid, we will mirror it later
-  half_grid := [3][5]bool{}
-  for i := 0; i < 3; i++ {
-    for j := 0; j < 5; j++ {
-      half_grid[i][j] = hex_array[(i) + (j)]
-    }
-  }
-  icon.Pixelmap = half_grid
+func makeHex(input string) string {
+	data := []byte(input)
+	sum := md5.Sum(data)
+	return hex.EncodeToString(sum[:])
 }
 
 // Build the pixel byte map
+// Currently just all filled-in
 func makePixels(square_size int) []byte {
-  pixels := make([]byte, square_size)
+	pixels := make([]byte, square_size)
 	for i := 0; i < square_size; i++ {
-    pixels[i] = 1
+		pixels[i] = 1
 	}
-  return pixels
+	return pixels
 }
 
 // Create colors based on a hex string
 func makeColors(hex string) []color.NRGBA {
-  // Primary
-  primary_red, _:= strconv.ParseInt(hex[0:2], 16, 32)
-  primary_blue, _:= strconv.ParseInt(hex[6:8], 16, 32)
-  primary_green, _:= strconv.ParseInt(hex[12:14], 16, 32)
-  color_primary := color.NRGBA{
-    R: uint8(primary_red),
-    G: uint8(primary_green),
-    B: uint8(primary_blue),
-    A: 0xff,
-  }
-
-  // White
-  white := color.NRGBA{0xf1, 0xf1, 0xf1, 0xff}
-
-  colors := []color.NRGBA{ white, color_primary }
-  return colors
+	// Primary
+	primary_red, _ := strconv.ParseInt(hex[0:2], 16, 32)
+	primary_blue, _ := strconv.ParseInt(hex[6:8], 16, 32)
+	primary_green, _ := strconv.ParseInt(hex[12:14], 16, 32)
+	color_primary := color.NRGBA{R: uint8(primary_red), G: uint8(primary_green), B: uint8(primary_blue), A: 0xff}
+	white := color.NRGBA{0xf1, 0xf1, 0xf1, 0xff}
+	colors := []color.NRGBA{white, color_primary}
+	return colors
 }
 
+// Turn a slice of letters into a nested array of booleans [3][5]bool
+// @TODO: Refactor this
+func makeHalfMap(hex string) [3][5]bool {
+	letters := strings.Split(hex, "")
+	hex_array := []bool{}
+
+	// Pair the letters by two's and convert the hex (e.g. f8) to integer
+	for i := 0; i < len(letters); i = i + 2 {
+		digit := letters[i : i+2]
+		num, _ := strconv.ParseInt(digit[0], 16, 32)
+		hex_array = append(hex_array, num%2 == 0)
+	}
+	hex_array = hex_array[:15]
+
+	half_grid := [3][5]bool{}
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 5; j++ {
+			half_grid[i][j] = hex_array[(i)+(j)]
+		}
+	}
+	return half_grid
+}
+
+// Mirror Half Grid
+// Turn the [3][5] array into a [6][5]
+func mirrorHalfGrid([3][5]bool) [6][5]bool {
+	return [6][5]bool{}
+}
 
 // Render the image and return the bytes
 func (icon *Identicon) Render() []byte {
-  // Create the hex and the map
-  icon.CreateHex()
-  icon.CreateMap()
+	// @TODO:
+	// Create the hex and the map
+	// Change this to:
+	// halfMap, hex := createMap(icon.Input)
+	// icon.Color = createColor(hex)
+	// icon.PixelMap = mirrorHalfMap(halfMap)
+	// ... go through the pixelmap and build the image bytes
+	// png.Encode and return the bytes
 
-  // Build the colors, rectagle, palette and the image
-  colors := makeColors(icon.Hex)
-  rect := image.Rect(0, 0, icon.Width, icon.Height)
-  palette := color.Palette{ colors[0], colors[1] }
-  img := image.NewPaletted(rect, palette)
+	icon.Hex = makeHex(icon.Input)
 
-  // Loop throught the pixelmap and build each square
-  for i := 0; i < len(icon.Pixelmap); i++ {
-    for j := 0; j < len(icon.Pixelmap[i]); j++ {
+	// icon.Pixelmap = mirrorHalfGrid(makeHalfMap(icon.Input))
+	icon.Pixelmap = makeHalfMap(icon.Input)
 
-      // If the square is true then color it
-      if icon.Pixelmap[i][j] {
-        // Go column by column by column and fill the square
-        for k := 0; k < (icon.SquareSize); k++ {
-          // create the x and y coordinates to start
-          x := icon.Padding + (i * icon.SquareSize)
-          y := icon.Padding + (j * icon.SquareSize) + k
-          offset := img.PixOffset(x, y)
-          copy(img.Pix[offset:], makePixels(icon.SquareSize))
+	// Build the colors, rectagle, palette and the image
+	colors := makeColors(icon.Hex)
+	rect := image.Rect(0, 0, icon.Width, icon.Height)
+	palette := color.Palette{colors[0], colors[1]}
+	img := image.NewPaletted(rect, palette)
 
-          // Mirror the process on the right side
-          x = icon.Padding + (5 - i) * icon.SquareSize // The 5 comes from the 5 row, this effect the column count
-          offset = img.PixOffset(x, y)
-          copy(img.Pix[offset:], makePixels(icon.SquareSize))
-        }
-      }
-    }
-  }
+	// Loop throught the pixelmap and build each square
+	for i := 0; i < len(icon.Pixelmap); i++ {
+		for j := 0; j < len(icon.Pixelmap[i]); j++ {
 
-  // Write the bytes to a buffer and return it the bytes
-  var buffer bytes.Buffer
-  png.Encode(&buffer, img)
-  return buffer.Bytes()
+			// If the square is true then color it
+			if icon.Pixelmap[i][j] {
+
+				// Go column by column by column and fill the square
+				for k := 0; k < (icon.SquareSize); k++ {
+
+					// create the x and y coordinates to start
+					x := icon.Padding + (i * icon.SquareSize)
+					y := icon.Padding + (j * icon.SquareSize) + k
+					offset := img.PixOffset(x, y)
+					copy(img.Pix[offset:], makePixels(icon.SquareSize))
+
+					// Mirror the process on the right side
+					x = icon.Padding + (5-i)*icon.SquareSize // The 5 comes from the 5 row, this effect the column count
+					offset = img.PixOffset(x, y)
+					copy(img.Pix[offset:], makePixels(icon.SquareSize))
+				}
+			}
+		}
+	}
+
+	// Write the bytes to a buffer and return it the bytes
+	var buffer bytes.Buffer
+	png.Encode(&buffer, img)
+	return buffer.Bytes()
 }
--- main.go
+++ main.go
@@ -1,56 +1,52 @@
 package main
 
-
 import (
-  "fmt"
-  "io/ioutil"
-  "os"
-  "log"
-  "github.com/Jumballaya/identicon/icon"
-  "github.com/urfave/cli"
+	"fmt"
+	"github.com/Jumballaya/identicon/icon"
+	"github.com/urfave/cli"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
 var output string
 
-
 func makeIdenticon(input string) {
-  icn := &icon.Identicon{ Input: input, Padding: 4, SquareSize: 82, Width: 500, Height: 418 }
-  png := icn.Render()
-  ioutil.WriteFile(output + "/" + input + ".png", png, os.FileMode(0644))
+	icn := &icon.Identicon{Input: input, Padding: 4, SquareSize: 82, Width: 500, Height: 418}
+	png := icn.Render()
+	ioutil.WriteFile(output+"/"+input+".png", png, os.FileMode(0644))
 }
 
 func main() {
-  output = "./"
+	output = "./"
 
-  app := cli.NewApp()
-  app.Name = "Identicon"
-  app.Usage = "Create Identicon images"
-  app.Version = "0.0.1"
+	app := cli.NewApp()
+	app.Name = "Identicon"
+	app.Usage = "Create Identicon images"
+	app.Version = "0.0.1"
 
-  app.Flags = []cli.Flag{
-    cli.StringFlag{
-      Name: "output, o",
-      Value: "./",
-      Usage: "output destination for the identicon images",
-      Destination: &output,
-    },
-  }
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "output, o",
+			Value:       "./",
+			Usage:       "output destination for the identicon images",
+			Destination: &output,
+		},
+	}
 
-  app.Action = func(c *cli.Context) error {
-    input := c.Args().Get(0)
-    if len(input) > 0 {
-      makeIdenticon(input)
-      fmt.Printf("\nIdenticon Created\n\n")
-    } else {
-      fmt.Printf("\nPlease enter input string\n\n")
-    }
-    return nil
-  }
+	app.Action = func(c *cli.Context) error {
+		input := c.Args().Get(0)
+		if len(input) > 0 {
+			makeIdenticon(input)
+			fmt.Printf("\nIdenticon Created\n\n")
+		} else {
+			fmt.Printf("\nError: Input required to make image\n\n")
+		}
+		return nil
+	}
 
-
-
-  err := app.Run(os.Args)
-  if err != nil {
-    log.Fatal(err)
-  }
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
